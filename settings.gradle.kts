d7f14802b9b8bd503e3c7a292312d6c20d9fa76f
younuspe <163322155+younuspe@users.noreply.github.com>
2026-08-14 08:50:56 +0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-
 pluginManagement {
     repositories {
         google {
@@ -14,6 +10,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 dependencyResolutionManagement {
